c0aa4bd636f7b7e50082bdf4f1b93faaad108d42
Daniel Gran <contact@grandaniel.com>
2026-03-12 16:17:26 +0100

    add: sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxt/eslint"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/icon",
+    "@nuxt/eslint",
+    "@nuxtjs/sitemap",
+  ],
   imports: {
     autoImport: false,
   },
